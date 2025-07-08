235af35159fa9c6fef6c981ce7acd74263d84c62
TarunBits <tarunbhattmortal@gmail.com>
2025-07-08 12:54:08 +0530

    #91 CRUD in React HTTP GET Method to Fetch & Display Data with Axios + Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useEffect } from "react";
-import { getPost } from "./api/PostApi";
+import { Posts } from "./components/Posts";
+import "./App.css";
 
 const App = () => {
-  const getPostData = async () => {
-    const res = await getPost();
-    console.log(res);
-  };
-
-  useEffect(() => {
-    getPostData();
-  });
+  return (
+    <section className="main-section">
+      <Posts />
+    </section>
+  );
 };
 
 export default App;
