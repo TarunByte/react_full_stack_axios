b71f7cad1a8327d925d87984e6a661187a1c4f53
TarunBits <tarunbhattmortal@gmail.com>
2025-07-08 13:44:54 +0530

    CRUD Operations in React Full Stack: Delete Data with Axios

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getPost } from "../api/PostApi";
+import { deletePost, getPost } from "../api/PostApi";
 
 export const Posts = () => {
   const [data, setData] = useState([]);
@@ -14,6 +14,23 @@ export const Posts = () => {
     getPostData();
   }, []);
 
+  // function to delete Post
+  const handleDeletePost = async (id) => {
+    try {
+      const res = await deletePost(id);
+      if (res.status === 200) {
+        const newUpdatedPosts = data.filter((curPost) => {
+          return curPost.id !== id;
+        });
+        setData(newUpdatedPosts);
+      } else {
+        console.log("Failed to delete the post:", res.status);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <section className="section-post">
       <ol>
@@ -24,7 +41,12 @@ export const Posts = () => {
               <p>Title: {title}</p>
               <p>Body: {body}</p>
               <button>Edit</button>
-              <button className="btn-delete">Delete</button>
+              <button
+                className="btn-delete"
+                onClick={() => handleDeletePost(id)}
+              >
+                Delete
+              </button>
             </li>
           );
         })}
