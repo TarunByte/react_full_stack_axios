c9ca7c845abc8d40415da005a137bf03f6f98e0c
TarunBits <tarunbhattmortal@gmail.com>
2025-07-10 15:57:00 +0530

    #93 Add Data to Real API & React6 Full Stack App with Axios POST method

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { deletePost, getPost } from "../api/PostApi";
+import { Form } from "./Form";
 
 export const Posts = () => {
   const [data, setData] = useState([]);
@@ -32,25 +33,30 @@ export const Posts = () => {
   };
 
   return (
-    <section className="section-post">
-      <ol>
-        {data.map((curElem) => {
-          const { id, body, title } = curElem;
-          return (
-            <li key={id}>
-              <p>Title: {title}</p>
-              <p>Body: {body}</p>
-              <button>Edit</button>
-              <button
-                className="btn-delete"
-                onClick={() => handleDeletePost(id)}
-              >
-                Delete
-              </button>
-            </li>
-          );
-        })}
-      </ol>
-    </section>
+    <>
+      <section className="section-form">
+        <Form data={data} setData={setData} />
+      </section>
+      <section className="section-post">
+        <ol>
+          {data.map((curElem) => {
+            const { id, body, title } = curElem;
+            return (
+              <li key={id}>
+                <p>Title: {title}</p>
+                <p>Body: {body}</p>
+                <button>Edit</button>
+                <button
+                  className="btn-delete"
+                  onClick={() => handleDeletePost(id)}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ol>
+      </section>
+    </>
   );
 };
