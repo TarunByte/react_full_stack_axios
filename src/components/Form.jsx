7c70652a244634d132c904129f14e2a88e527e72
TarunBits <tarunbhattmortal@gmail.com>
2025-07-12 03:55:28 +0530

    CRUD in React Update API Data & Reflect Changes with Axios PUT

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,13 @@
-import { useEffect, useId, useState } from "react";
-import { postData } from "../api/PostApi";
+import { useEffect, useState } from "react";
+import { postData, updateData } from "../api/PostApi";
 
 export const Form = ({ data, setData, updateDataApi, setUpdateDataApi }) => {
   const [addData, setAddData] = useState({
     title: "",
     body: "",
   });
+
+  let isEmpty = Object.keys(updateDataApi).length === 0;
 
   // get the updated Data and add into input field
   useEffect(() => {
@@ -31,17 +33,43 @@ export const Form = ({ data, setData, updateDataApi, setUpdateDataApi }) => {
   const addPostData = async () => {
     const res = await postData(addData);
     console.log("res", res);
+
     if (res.status === 201) {
       setData([...data, res.data]);
       setAddData({ title: "", body: "" });
-      console.log("resAfter", res.data);
+    }
+  };
+
+  // updatePostData
+  const updatePostData = async () => {
+    try {
+      const res = await updateData(updateDataApi.id, addData);
+      console.log(res);
+
+      if (res.status === 200) {
+        setData((prev) => {
+          return prev.map((curElem) => {
+            return curElem.id === res.data.id ? res.data : curElem;
+          });
+        });
+
+        setAddData({ title: "", body: "" });
+        setUpdateDataApi({});
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
   // form submission
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    addPostData();
+    const action = e.nativeEvent.submitter.value;
+    if (action === "Add") {
+      addPostData();
+    } else if (action === "Edit") {
+      updatePostData();
+    }
   };
 
   return (
@@ -71,7 +99,9 @@ export const Form = ({ data, setData, updateDataApi, setUpdateDataApi }) => {
           onChange={handleInputChange}
         />
       </div>
-      <button type="submit">Add</button>
+      <button type="submit" value={isEmpty ? "Add" : "Edit"}>
+        {isEmpty ? "Add" : "Edit"}
+      </button>
     </form>
   );
 };
